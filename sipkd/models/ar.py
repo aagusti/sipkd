0a4d2d463ada4838b32bd68099777794d8b54f1d
Owo Sugiana <sugiana@gmail.com>
2009-03-28 15:35:37 +0700

    final

--- sipkd/models/ar.py
+++ sipkd/models/ar.py
@@ -188,7 +188,50 @@ class ARPaymentPbb(DefaultModel, Base):
     UniqueConstraint('tahun', 'unit_kd', 'kode',
                 name = 'ar_payment_pbb_ukey')
 
-
+  
+class ARPaymentPbbRekap(DefaultModel, Base):
+    __tablename__  = 'ar_payment_pbb_rekap'
+    __table_args__ = {'extend_existing':True, 'schema' : schematbl,}
+    tahun           = Column(Integer, nullable=False)
+    unit_kd         = Column(String,  nullable=False)
+    thn_pajak_sppt  = Column(String(4))
+    unit_nm         = Column(String(128))
+    kode            = Column(String(50)) # generator PBB-YYYYYMMDD-BANKTGL-PERSESI-TP
+    #ama            = Column(String(255)) # nama pembayar
+    #lamat          = Column(String(255))
+    uraian          = Column(String(255)) #uraian payment
+    tgl_trans       = Column(Date)
+    tgl_validasi    = Column(Date)
+    rekening_kd     = Column(String(16))
+    rekening_nm     = Column(String(128))
+    pokok           = Column(BigInteger)
+    denda           = Column(BigInteger)
+    bunga           = Column(BigInteger)
+    nilai           = Column(BigInteger, nullable=False)
+    #status          = Column(SmallInteger, nullable=False, default=0)
+    sumber_id       = Column(SmallInteger, default=1)#1, 2, 3, 4
+    sumber_nm       = Column(String(16))#1, 2, 3, 4
+    posted          = Column(SmallInteger, nullable=False, default=0)
+    #ef_kode         = Column(String(32), unique=True) # no bayar
+    #ef_nama         = Column(String(64)) # nama wp
+    #ecamatan_kd     = Column(String(32))
+    #ecamatan_nm     = Column(String(64))
+    #elurahan_kd     = Column(String(32))
+    #elurahan_nm     = Column(String(64))
+    #ahun            = Column(Integer)
+    #ulan            = Column(Integer)
+    #inggu           = Column(Integer)
+    #ari             = Column(Integer)
+    #pwpd            = Column(String(32))
+    #th_tempo        = Column(Date)
+    is_kota          = Column(SmallInteger)
+    created  = Column(DateTime, nullable=False, default=datetime.utcnow)
+    updated  = Column(DateTime, nullable=True)
+    create_uid  = Column(Integer, nullable=False, default=1)
+    update_uid  = Column(Integer, nullable=True)
+    UniqueConstraint('tahun', 'unit_kd', 'kode',
+            name = 'ar_payment_pbb_rekap_ukey')
+    
 class ARInvoiceBphtb(DefaultModel, Base):
     __tablename__  = 'ar_invoice_bphtb'
     __table_args__ = {'extend_existing':True, 'schema' : schematbl,}
